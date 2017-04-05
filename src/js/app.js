7aafb756791e0217fb811fe6209e932001226c83
Fabricio Mouzo <fabricio.mouzo@rga.com>
2017-04-05 12:45:29 -0300

    Clean some files

--- src/js/app.js
+++ src/js/app.js
@@ -4,14 +4,30 @@ class App {
   constructor() {
   }
   init() {
-  console.log('initialized');
-    $('body').append('<p>Initialized</p>');
-    var renderer = PIXI.autoDetectRenderer(256, 256);
-    document.body.appendChild(renderer.view);
-    var stage = new PIXI.Container();
-  	renderer.render(stage);
+    this.width =  window.innerWidth; 
+    this.height = window.innerHeight;  
+    this.initPixi();
+    
+  }
+  initPixi = function() {
+    console.log(this.width);
+    console.log(this.height);
+    this.pixiApp = new PIXI.Application(this.width, this.height, {backgroundColor : 0x000000});
+    document.body.appendChild(this.pixiApp.view);
+    this.pixiContainer = new PIXI.Container();
+    window.onresize = this.pixiResize();
+    
+  }
+  
+  pixiResize = function() {
+    this.width = window.innerWidth;    
+    this.height = window.innerHeight;
+    this.pixiApp.renderer.view.style.width = this.width + 'px';
+    this.pixiApp.renderer.view.style.height = this.height + 'px';    
   }
 }
 
-const app = new App();
-app.init();
+document.addEventListener("DOMContentLoaded", function() {
+  const app = new App();
+  app.init();
+});
