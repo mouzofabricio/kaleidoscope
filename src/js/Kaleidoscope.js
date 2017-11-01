f7efe45676878a153162d9de2b8dca93fe23af70
Fabricio Mouzo <fabriciomouzo@Fabricios-MacBook-Air.local>
2017-11-01 01:13:08 -0300

    add interactivity and fix pattern scale

--- src/js/Kaleidoscope.js
+++ src/js/Kaleidoscope.js
@@ -3,7 +3,6 @@ import Pixi from 'pixi.js';
 export class Kaleidoscope {
     
     constructor(pixiApp) {
-        console.log("Kaleidoscope");
         this.HALF_PI = Math.PI / 2;
         this.TWO_PI = Math.PI * 2;
         this.pixiApp = pixiApp;
@@ -16,29 +15,32 @@ export class Kaleidoscope {
         this.zoom = 1.0;
         this.posX = window.innerWidth / 2;
         this.posY = window.innerHeight / 2;
-        //
         this.step = this.TWO_PI / this.slices;
         this.arcs = []
         this.spriteTiles = [];
         this.containers = [];
-
-        this.image = PIXI.Texture.fromImage('/images/base.jpg');
+        this.image = PIXI.Texture.fromImage('/images/primus.jpg');
         this.count = 0;
-
-       
-
-
+        this.interactiveMode = false;
+        this.mouseX = 0;
+        this.mouseY = 0;
     }
 
-    draw = function() {
+    draw() {
         let mainContainer = new PIXI.Container();
+        mainContainer.interactive = true;
+        mainContainer.mousemove = e => {
+            const {x,y} = e.data.global;
+            this.mouseX = x;
+            this.mouseY = y;
+        }
         for(let i = 0; i < this.slices; i++) {
             let arc = new PIXI.Graphics();
             let spriteTileArc = new PIXI.extras.TilingSprite(this.image,1500 * 2 ,1500 * 2);
             const currentStep = this.step * i + 1;
             arc.beginFill('0x000000');
             arc.moveTo(this.posX,this.posY);
-            arc.arc(this.posX,this.posY, this.radius * 1.5, -.5 * this.step, .5 * this.step);
+            arc.arc(this.posX,this.posY, this.radius * 1.3, -.5 * this.step, .5 * this.step);
             arc.endFill();
             spriteTileArc.mask = arc;
             var container = new PIXI.Container();
@@ -47,6 +49,7 @@ export class Kaleidoscope {
             container.pivot.x = this.posX;
             container.pivot.y = this.posY;
             container.rotation = -currentStep;
+            container.scale.x = i % 2 ? 1 : -1;
             this.spriteTiles.push(spriteTileArc);
             this.arcs.push(arc);
             this.containers.push(container);
@@ -59,16 +62,24 @@ export class Kaleidoscope {
 
          this.pixiApp.ticker.add(() => {
 
-            this.count += 0.0005;
-            // this.draw();
+            this.count += this.interactiveMode ? 0.5 : 0.005;
             for(let i = 0;i < this.spriteTiles.length; i++) {   
-                // this.arc[i].
-                // this.spriteTiles[i].tileScale.x = 0.1 + Math.sin(this.count);
-                // this.spriteTiles[i].tileScale.y = 0.1 + Math.cos(this.count);
-                this.spriteTiles[i].tilePosition.x += Math.sin(this.count);
-                this.spriteTiles[i].tilePosition.y += Math.cos(this.count);
+                if(this.interactiveMode) {
+                    // this.spriteTiles[i].tilePosition.x = this.mouseX + Math.sin(this.count);
+                    this.spriteTiles[i].tilePosition.x = this.mouseX + this.count;
+                    // this.spriteTiles[i].tilePosition.y = this.mouseY + Math.cos(this.count);
+                    this.spriteTiles[i].tilePosition.y = this.mouseY + this.count;
+                } else {
+                    this.spriteTiles[i].tilePosition.x += Math.sin(this.count);
+                    this.spriteTiles[i].tilePosition.y += Math.cos(this.count);
+                }
             }
         });
         
+    }
+
+    toggleInteractiveMode() {
+        console.log('toggleinteractive', this.interactiveMode);
+        this.interactiveMode = !this.interactiveMode;
     }
 }
--- src/js/app.js
+++ src/js/app.js
@@ -10,26 +10,37 @@ class App {
     this.initPixi();
     
   }
-  initPixi = function() {
+  initPixi() {
     console.log(this.width);
     console.log(this.height);
     this.pixiApp = new PIXI.Application(this.width, this.height, {backgroundColor : 0xffffff});
     document.body.appendChild(this.pixiApp.view);
     this.pixiContainer = new PIXI.Container();
     window.onresize = this.pixiResize();
+
     this.kaleidoscope = new Kaleidoscope(this.pixiApp);
+    this.addListeners();
     this.kaleidoscope.draw();
   }
   
-  pixiResize = function() {
+  pixiResize() {
     this.width = window.innerWidth;    
     this.height = window.innerHeight;
     this.pixiApp.renderer.view.style.width = this.width + 'px';
     this.pixiApp.renderer.view.style.height = this.height + 'px';    
   }
+
+  addListeners() {
+    document.addEventListener('keyup', e => {
+      if(e.keyCode === 32) {
+        this.kaleidoscope.toggleInteractiveMode();
+      }
+    });
+
+  }
 }
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", () => {
   const app = new App();
   app.init();
 });
